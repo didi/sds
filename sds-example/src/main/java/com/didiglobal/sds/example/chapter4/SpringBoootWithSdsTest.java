46ae84ab20ee5ac010cac5aee27a29592c8ef6d0
manzhizhen <835576511@qq.com>
2020-03-29 21:35:52 +0800

    添加Spring Boot接入SDS的example

---
+++ sds-example/src/main/java/com/didiglobal/sds/example/chapter4/SpringBoootWithSdsTest.java
@@ -0,0 +1,53 @@
+package com.didiglobal.sds.example.chapter4;
+
+import com.didiglobal.sds.client.exception.SdsException;
+import com.didiglobal.sds.easy.SdsEasyUtil;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 运行Spring Boot的单元测试来验证结果
+ *
+ * @Author: yizhenqiang
+ * @Date: Create in 2020-03-29 18:34
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoootWithSdsTest {
+
+    @Autowired
+    private OrderManageService orderManageService;
+
+    @org.junit.Test
+    public void test() {
+        int times = 2000;
+        while (times-- > 0) {
+            Long orderId = null;
+            try {
+                orderId = orderManageService.createOrder(12345L, "杭州西湖区西溪谷G座");
+                System.out.println("createOrder新创建的订单id:" + orderId);
+
+            } catch (SdsException e) {
+                System.out.println("createOrder此次调用已经被降级！");
+            }
+        }
+
+
+        times = 2000;
+        while (times-- > 0) {
+            Long orderId = null;
+            try {
+
+                orderId = SdsEasyUtil.invokerMethod(OrderManageService.CREATE_ORDER_POINT_1, null,
+                        ()-> orderManageService.createOrder1(12345L, "杭州西湖区西溪谷G座"));
+
+                System.out.println("createOrder1新创建的订单id:" + orderId);
+
+            } catch (SdsException e) {
+                System.out.println("createOrder1此次调用已经被降级！");
+            }
+        }
+    }
+}
