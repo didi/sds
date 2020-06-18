25345beff2a81ac2204d4260aff85ffdf9d47c4c
luobinbin5 <luobinbin5@jd.com>
2020-06-18 22:36:09 +0800

    fix: 修改线上build配置

--- sds-front/vue.config.js
+++ sds-front/vue.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   outputDir: path.resolve(__dirname, "target/static"),
+  productionSourceMap: false,
   devServer: {
     port: 8080,
     host: "localhost",
